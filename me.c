cd2b1b4751e75c250212dcb15c3e1b056f7e083e
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-14 02:57:50 +0000

    run this

---
+++ me.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * _printf - function for formatted printing
+ * @const - argument input
+ * @char - argument input
+ * @*format - input to be printed
+ * @... - ellipsis for variadic function
+ * Return: Always 0 (Success).
+ */
+int _printf(const char *format, ...)
+{
+	int F;
+
+	char *random;
+
+	if (*format == '\0')
+	{
+		printf("format for me is %c\n", F);
+		return (0);
+	}
+	else
+	{
+		printf("format for me is %s\n", random);
+		return (0);
+	}
+		return (0);
+}
