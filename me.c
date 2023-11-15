86f34ea4d96cdc2ff49841f76eec4db3b6ab25a1
iMGamal <mohamed.gamal126027@gmail.com>
2023-11-15 00:13:09 +0000

    0.01%

--- me.c
+++ me.c
@@ -1,38 +1,30 @@
+#include <limits.h>
 #include <stdio.h>
-#include "main.h"
 #include <stdarg.h>
+#include "main.h"
 
 /**
- * _printf - a function that produces output according to a format.
- *
- * @const - argument input
- * @char * - argument input
- * @format - a pointer.
- * @... - ellipsis for variadic function
- * Return: Always 0 (Success).
+ *_printf - a function that produces output according to a format.
+ *@const - data input
+ *@char * - data input
+ *@format - a character string.
+ *Return : the number of characters printed (excluding the null byte used to end output to strings).
  */
-
 int _printf(const char *format, ...)
 {
-	int i;
-	va_list percent;
+	int i = 0;
 
-	i = 0;
-
-	if (*format == 0)
+	va_list ap;
+	va_start(ap, format);
+	if (format == 0)
 	{
-		printf("format for me is %c\n", i);
+		printf("Character:[%c]\n", i);
 		return (0);
 	}
-	else if (*format > 0)
+	else if (format != 0)
 	{
-		printf("string is a loose %s\n", format);
+		printf("String:[%s]\n", format);
 		return (0);
 	}
-	va_start(percent, *format);
-	{
-		printf("for us it is relative %s\n", percent);
-		return (0);
-	}
-		return (0);
+	return (0);
 }
