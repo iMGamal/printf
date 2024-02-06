c50f5b7ac7b80130a2255b508a4f9baca9884e8f
iMGamal <mohamed.gamal126027@gmail.com>
2024-02-06 13:05:44 +0000

    Jay

--- printf.c
+++ printf.c
@@ -9,30 +9,66 @@
  * @format: character string
  * Return - int
  */
+int percent_c(int c)
+{
+	return (write(1, &c, 1));
+}
+int percent_s(char *string)
+{
+	return (write(1, string, 1));
+}
+int percent_d(int y)
+{
+	return (write(1, &y, 1));
+}
+int percent_i(int z)
+{
+	return (write(1, &z, 1));
+}
+
+int specifier(char percent, va_list ap)
+{
+	int x = 0;
+
+	if (percent == 'c')
+	{
+		x += percent_c(va_arg(ap, int));
+	}
+	else if (percent == 's')
+	{
+		x += percent_s(va_arg(ap, char *));
+	}
+	else if (percent == 'd')
+	{
+		x += percent_d(va_arg(ap, int));
+	}
+	else if (percent == 'i')
+	{
+		x += percent_i(va_arg(ap, int));
+	}
+	return (x);
+}
+
 int _printf(const char *format, ...)
 {
 	va_list ap;
 
-	int i;
+	int x;
 
 	va_start(ap, format);
-	i = 0;
+	x = 0;
 	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
-			i += _printf(format++, ap);
+			x += _printf(format++, ap);
 		}
 		else
 		{
-			i += write(1, format, 1);
+			x += write(1, format, 1);
 			format++;
 		}
 	}
-	if (*format == 'c')
-	{
-		i += _printf(format++, ap);
-	}
 	va_end(ap);
-	return (i);
+	return (x);
 }
