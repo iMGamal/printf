beacd7b218d665bcd3b3a317b229c9fdfb8a2746
iMGamal <mohamed.gamal126027@gmail.com>
2024-02-06 14:10:28 +0000

    30

--- printf.c
+++ printf.c
@@ -9,46 +9,6 @@
  * @format: character string
  * Return - int
  */
-int percent_c(int c)
-{
-	return (write(1, &c, 1));
-}
-int percent_s(char *string)
-{
-	return (write(1, string, 1));
-}
-int percent_d(int y)
-{
-	return (write(1, &y, 1));
-}
-int percent_i(int z)
-{
-	return (write(1, &z, 1));
-}
-
-int specifier(char percent, va_list ap)
-{
-	int x = 0;
-
-	if (percent == 'c')
-	{
-		x += percent_c(va_arg(ap, int));
-	}
-	else if (percent == 's')
-	{
-		x += percent_s(va_arg(ap, char *));
-	}
-	else if (percent == 'd')
-	{
-		x += percent_d(va_arg(ap, int));
-	}
-	else if (percent == 'i')
-	{
-		x += percent_i(va_arg(ap, int));
-	}
-	return (x);
-}
-
 int _printf(const char *format, ...)
 {
 	va_list ap;
@@ -68,6 +28,30 @@ int _printf(const char *format, ...)
 			x += write(1, format, 1);
 			format++;
 		}
+	}
+	while (*format == '\0')
+	{
+		if (*format == 'c')
+		{
+			x += _printf(va_arg(ap, char *));
+		}
+		else if (*format == 's')
+		{
+			x += _printf(va_arg(ap, char *));
+		}
+		else if (*format == 'd')
+		{
+			x += _printf(va_arg(ap, char *));
+		}
+		else if (*format == 'i')
+		{
+			x += _printf(va_arg(ap, char *));
+		}
+		else
+		{
+			write(1, format, 1);
+		}
+	format++;
 	}
 	va_end(ap);
 	return (x);
